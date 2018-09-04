749b46887eb0c111b7cf9f7994a6dd24369dd2c7
Luciano Dexheimer Morais <lucianodmorais@gmail.com>
2018-09-04 11:32:53 -0300

    versao

---
+++ www/platforms/browser/www/js/registration.js
@@ -0,0 +1,95 @@
+// JavaScript Document
+// ANDROID API KEY AIzaSyBWaodrtQpBb6KxujbV2XfKQwLJAjN7Jss
+// ANDROID PROJECT NUMBER 822409998369
+var ref="";
+var app = {
+    // Application Constructor
+    initialize: function() {
+		alert('a');
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        console.log('Received Device Ready Event');
+        console.log('calling setup push');
+        app.setupPush();
+    },
+    setupPush: function() {
+        console.log('calling push init');
+        var push = PushNotification.init({
+            "android": {
+                "senderID": "822409998369"
+            },
+            "browser": {},
+            "ios": {
+                "sound": true,
+                "vibration": true,
+                "badge": true,
+				"clearBadge":true
+            }
+        });
+        console.log('after init');
+
+        push.on('registration', function(data) {
+			logar();
+			
+			//alert(device.platform);
+            var oldRegId = localStorage.getItem('registrationId');
+            if (oldRegId !== data.registrationId) {
+                // Save new registration ID
+                localStorage.setItem('registrationId', data.registrationId);
+                // Post registrationId to your app server as the value has changed
+            }
+			
+			 var oldDevice = localStorage.getItem('device');
+            if (oldDevice !== device.platform) {
+                // Save new registration ID
+                localStorage.setItem('device', device.platform);
+                // Post registrationId to your app server as the value has changed
+            }
+
+           // var parentElement = document.getElementById('registration');
+           // var listeningElement = parentElement.querySelector('.waiting');
+           // var receivedElement = parentElement.querySelector('.received');
+
+            //listeningElement.setAttribute('style', 'display:none;');
+            //receivedElement.setAttribute('style', 'display:block;');
+        });
+
+        push.on('error', function(e) {
+			alert('erro');
+            console.log("push error = " + e.message);
+        });
+
+        push.on('notification', function(data) {
+		//	alert('event');
+            console.log('notification event');
+			if(data.message!==null && data.message!="" && data.message!="null") {
+            	navigator.notification.alert(
+					data.message,         // message
+					function(){
+						//alert('vai teste');
+							if(localStorage.id!=undefined && localStorage.senha!=undefined && localStorage.usuario!=undefined) {
+								//alert('logado');
+							} else {
+								//alert('nao');
+							}
+							//var ref = window.open('http://192.241.205.6/vitaclub/', '_blank', 'toolbar=no,location=no');
+						},                 // callback
+					'Vitaclub',           // title
+					'Ok'                  // buttonName
+				);
+			}
+       });
+    }
+};
---
+++ www/www/js/dashboard.js
@@ -0,0 +1,27 @@
+function logar() {
+	
+	alert('load');
+	$('#load').load('http://192.241.205.6/vitaclub/usuarios/loginCookie.php',{'auth_usuario':localStorage.usuario,'auth_senha':localStorage.senha,'regID':localStorage.registrationId,'device':localStorage.device},function(){
+		//alert('teste');
+		console.log(localStorage.device+' <');
+		var ref = cordova.InAppBrowser.open('http://192.241.205.6/vitaclub/', '_blank', 'toolbar=no,location=no,transitionstyle=crossdissolve');
+		ref.addEventListener('loadstart', function(event) {
+			//alert(event.url);
+			 if (event.url.match("mobile/close") || event.url.match("sair.php")) {
+				ref.close();
+				localStorage.clear();
+				window.location.replace('index.html');
+			 }
+		 }); 
+		 ref.addEventListener('exit', function(event) {
+			localStorage.clear();
+			window.location.replace('index.html');
+		 });
+	});
+}
+$(function(){
+	//alert('ok2 ->'+localStorage.usuario);
+	//alert(localStorage.usuario+" -> "+localStoage.senha);
+	
+	
+});
